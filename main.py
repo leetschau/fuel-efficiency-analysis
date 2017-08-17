7a3648b168d9b35083b1ca392c6026f9c7c8a639
Li Chao <leetschau@gmail.com>
2017-08-17 14:33:19 +0800

    all 11 steps verified

--- main.py
+++ main.py
@@ -4,11 +4,14 @@
 Created on Wed Aug 16 12:13:11 2017
 
 @author: leo
+
+This script is the implementation of chapter 7 "Driving Visual Analyses with Automobile Data" 
+of "Practical Data Science Cookbook" by Tony Ojeda, etc.
 """
 #%%
 import pandas as pd
 import numpy as np
-#from ggplot import *
+
 vehicles = pd.read_csv("vehicles.csv")
 
 column_names = vehicles.columns.values
@@ -32,10 +35,11 @@ averaged = grouped['comb08', 'highway08', 'city08'].agg([np.mean])
 averaged.columns = ['comb08_mean', 'highway08_mean', 'city08_mean']
 averaged['year'] = averaged.index
 
-print(ggplot(averaged,
-             aes('year', 'comb08_mean'))
-                 + geom_point(color='steelblue')
-                 + xlab('Year') + ylab('Average MPG') + ggtitle('All cars'))
+print(ggplot(averaged, aes('year', 'comb08_mean')) +
+      geom_point(color='steelblue') +
+      xlab('Year') +
+      ylab('Average MPG') +
+      ggtitle('All cars'))
 
 #%% step 5
 criteria1 = vehicles.fuelType1.isin(['Regular Gasoline', 'Prenium Gasoline', 'Midgrade Gasoline'])
@@ -48,3 +52,32 @@ len(vehicles_non_hybrid)
 grouped = vehicles_non_hybrid.groupby(['year'])
 averaged = grouped['comb08'].agg([np.mean])
 print(averaged)
+
+#%% step 7 ~ 9
+pd.unique(vehicles_non_hybrid.displ)
+
+criteria = vehicles_non_hybrid.displ.notnull()
+vehicles_non_hybrid = vehicles_non_hybrid[criteria]
+vehicles_non_hybrid.displ = vehicles_non_hybrid.displ.astype('float')
+
+criteria = vehicles_non_hybrid.comb08.notnull()
+vehicles_non_hybrid = vehicles_non_hybrid[criteria]
+vehicles_non_hybrid.comb08 = vehicles_non_hybrid.comb08.astype('float')
+
+print(ggplot(vehicles_non_hybrid, aes('displ', 'comb08')) +
+      geom_point(color='steelblue') +
+      xlab('Engine Displacement') +
+      ylab('Average MPG') +
+      ggtitle('Gasoline cars'))
+
+#%% step 10
+grouped_by_year = vehicles_non_hybrid.groupby(['year'])
+avg_grouped_by_year = grouped_by_year['displ', 'comb08'].agg([np.mean])
+
+#%% step 11
+avg_grouped_by_year['year'] = avg_grouped_by_year.index
+melted_avg_grouped_by_year = pd.melt(avg_grouped_by_year, id_vars='year')
+
+from ggplot import facet_wrap
+p = ggplot(aes(x='year', y='value', color='variable_0'), data=melted_avg_grouped_by_year)
+p + geom_point() + facet_wrap('variable_0')
